4d61a685a257d6458f2241da8b21e0cbc3893b1b
graveetone <zastavskijs@gmail.com>
2023-03-14 19:15:08 +0200

    add internet speed table and related associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_164927) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_165502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "internet_speeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.decimal "download_speed", precision: 15, scale: 2, null: false
+    t.string "download_units", null: false
+    t.uuid "place_id", null: false
+    t.index ["download_units"], name: "index_internet_speeds_on_download_units"
+    t.index ["place_id"], name: "index_internet_speeds_on_place_id"
+  end
 
   create_table "places", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
@@ -22,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_164927) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "internet_speeds", "places"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Resetting database...'
+InternetSpeed.delete_all
+Place.delete_all
+puts 'Database is clear!'
+
+puts 'Seeding places'
+
+Place.create(
+    [
+        {
+            name: 'Starbucks',
+            address: '123 Jump Street',
+            city: 'Los Angeles'
+        },
+        {
+            name: 'Aroma Kava',
+            address: 'Naukova Street',
+            city: 'Lviv'
+        }
+    ]
+)
+puts 'Places are created'
+
+puts 'Seeding internet speeds'
+
+InternetSpeed.create(
+    [
+        {
+            place_id: Place.first.id,
+            download_speed: 28.45,
+            download_units: 'mbps'
+        },
+        {
+            place_id: Place.first.id,
+            download_speed: 19.94,
+            download_units: 'mbps'
+        },
+        {
+            place_id: Place.last.id,
+            download_speed: 29.67,
+            download_units: 'mbps'
+        }
+    ]
+)
+puts 'Internet speeds created!'
