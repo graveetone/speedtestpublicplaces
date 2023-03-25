16d9df6bc27ee0317525d47e701887b5f93683d0
graveetone <zastavskijs@gmail.com>
2023-03-25 12:40:00 +0200

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
 puts 'Resetting database...'
 InternetSpeed.delete_all
@@ -12,42 +6,21 @@ Place.delete_all
 puts 'Database is clear!'
 
 puts 'Seeding places'
-
-Place.create(
-    [
-        {
-            name: 'Starbucks',
-            address: '123 Jump Street',
-            city: 'Los Angeles'
-        },
-        {
-            name: 'Aroma Kava',
-            address: 'Naukova Street',
-            city: 'Lviv'
-        }
-    ]
-)
+5.times do
+    Place.create(
+        name: Faker::Company.name,
+        address: Faker::Address.street_address,
+        city: Faker::Address.city
+    )
+end
 puts 'Places are created'
 
 puts 'Seeding internet speeds'
-
-InternetSpeed.create(
-    [
-        {
-            place_id: Place.first.id,
-            download_speed: 28.45,
-            download_units: 'mbps'
-        },
-        {
-            place_id: Place.first.id,
-            download_speed: 19.94,
-            download_units: 'mbps'
-        },
-        {
-            place_id: Place.last.id,
-            download_speed: 29.67,
-            download_units: 'mbps'
-        }
-    ]
-)
+5.times do
+    InternetSpeed.create(
+        place_id: Place.all.ids.sample,
+        download_speed: Faker::Number.decimal(l_digits:2, r_digits:2),
+        download_units: 'mbps'
+    )
+end
 puts 'Internet speeds created!'
